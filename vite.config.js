5c5120928d489131a65cfb576101f967d487ac94
hussamshannan <info@shark-plan.com>
2025-07-08 17:48:47 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
     },
   },
 });
-
