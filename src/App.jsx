80b60ad745a15a036d71915de1033a819e9bd512
hussamshannan <info@shark-plan.com>
2025-07-20 02:48:39 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -86,6 +86,14 @@ function App() {
           Object.entries(fetchedColors).forEach(([key, val]) => {
             document.documentElement.style.setProperty(key, val);
           });
+           const metaTag = document.querySelector('meta[name="theme-color"]');
+           const primaryClr = getComputedStyle(document.documentElement)
+             .getPropertyValue("--primary-clr")
+             .trim();
+
+           if (metaTag && primaryClr) {
+             metaTag.setAttribute("content", primaryClr);
+           }
         }
       } catch (err) {
         console.error("Error fetching colors:", err);
@@ -189,14 +197,7 @@ function App() {
         setFeedbacks(res.data);
 
 
-         const metaTag = document.querySelector('meta[name="theme-color"]');
-         const primaryClr = getComputedStyle(document.documentElement)
-           .getPropertyValue("--primary-clr")
-           .trim();
-
-         if (metaTag && primaryClr) {
-           metaTag.setAttribute("content", primaryClr);
-         }
+        
       } catch (err) {
         setContent(null);
       } finally {
